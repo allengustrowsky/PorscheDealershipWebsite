87e20a8bcf414e8af7772b21b97471758e606b0c
Allen Gustrowsky <allen@may21.us>
2022-03-18 17:20:54 -0400

    Initialized repo, now adding all the files I have so far to it (index page and the associated js, css, and image files)

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+const items = document.querySelectorAll(".navbar-item2>span")
+items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        console.log("hovered over")
+        item.style.cursor = "pointer"
+        item.style.backgroundColor = "black"
+        item.style.color = "white"
+    })
+    item.addEventListener("mouseout", () => {
+        item.style.cursor = "none"
+        item.style.backgroundColor = ""
+        item.style.color = "black"
+    })
+    
+})
+
+//pointer type
